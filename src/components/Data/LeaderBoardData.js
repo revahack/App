6125286872415188945e3b398f3a77f7c12af677
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-11-08 22:02:51 +0530

    Updated Leaderboard

--- src/components/Data/LeaderBoardData.js
+++ src/components/Data/LeaderBoardData.js
@@ -1,286 +1,382 @@
 const LeaderBoardData = [
-    {
-      "id": 0,
-      "name": "#include",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 1,
-      "name": "ASPB",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 2,
-      "name": "ASPB",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 3,
-      "name": "Aurora",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 4,
-      "name": "Binary",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 5,
-      "name": "CodePetrolDelta",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 6,
-      "name": "coding ninja",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 7,
-      "name": "Coffee and code",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 8,
-      "name": "Debugger Warriors",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 9,
-      "name": "Dragons",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 10,
-      "name": "Encephlon",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 11,
-      "name": "Error404",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 12,
-      "name": "Flash",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 13,
-      "name": "Green.io",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 14,
-      "name": "Hack Elite",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 15,
-      "name": "hack_reva",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 16,
-      "name": "HackStreet Boys",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 17,
-      "name": "Haxxor Bois",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 18,
-      "name": "Horus",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 19,
-      "name": "Mind Hack Zombies",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 20,
-      "name": "NBK BOOMERS",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 21,
-      "name": "PNS",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 22,
-      "name": "PTLM",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 23,
-      "name": "Run Time Error",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 24,
-      "name": "Segmentation Fault",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 25,
-      "name": "Syntax Sanitizers",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 26,
-      "name": "The Rebels",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 27,
-      "name": "VFour",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 28,
-      "name": "wolves",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 29,
-      "name": "A_square",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 30,
-      "name": "Complexity",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 31,
-      "name": "CREATORS",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 32,
-      "name": "Droid Squad",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 33,
-      "name": "Entropy",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 34,
-      "name": "Hack Elite",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 35,
-      "name": "HACKATHON A",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 36,
-      "name": "Hackers",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 37,
-      "name": "I.G.I.",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 38,
-      "name": "Immortal",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 39,
-      "name": "JUST CHILLING (?^_^)?",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 40,
-      "name": "Ovijeet",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 41,
-      "name": "Saap",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 42,
-      "name": "TechFlurry's",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 43,
-      "name": "TECHNOVERTS",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 44,
-      "name": "The I.T. Guys",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 45,
-      "name": "The Scrapers",
-      "questions": 1,
-      "points": 10
-    },
-    {
-      "id": 46,
-      "name": "WB",
-      "questions": 1,
-      "points": 10
-    }
-]
+  {
+    "id": 0,
+    "name": "Shivani Saha",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 1,
+    "name": "ASPB",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 2,
+    "name": "Immortal",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 3,
+    "name": "HACKATHON G",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 4,
+    "name": "HACKATHON A",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 5,
+    "name": "Code Stomp",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 6,
+    "name": "awake ? code : sleep",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 7,
+    "name": "unstoppable coders",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 8,
+    "name": "Complexity",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 9,
+    "name": "VVVR",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 10,
+    "name": "Troopers",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 11,
+    "name": "HACK ELITE",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 12,
+    "name": "CSE_5516",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 13,
+    "name": "Hunter Squad",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 14,
+    "name": "The Terminators",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 15,
+    "name": "US",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 16,
+    "name": "Fine Crew",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 17,
+    "name": "Hackaholic",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 18,
+    "name": "Reva shopping",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 19,
+    "name": "Droid Squad",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 20,
+    "name": "QUINTET",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 21,
+    "name": "Saap",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 22,
+    "name": "Lil Coders",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 23,
+    "name": "Z2VY",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 24,
+    "name": "Coders123",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 25,
+    "name": "Alpha team",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 26,
+    "name": "Team Un-Grads",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 27,
+    "name": "AppVision",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 28,
+    "name": "New Icons",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 29,
+    "name": "TechFlurry's",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 30,
+    "name": "Trio",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 31,
+    "name": "Innovation Geeks",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 32,
+    "name": "CREATORS",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 33,
+    "name": "The core four",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 34,
+    "name": "Team blitz",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 35,
+    "name": "NBK BOOMERS",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 36,
+    "name": "Adreeja",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 37,
+    "name": "THE BRO CODE",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 38,
+    "name": "Hack AI Elite",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 39,
+    "name": "Aviators",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 40,
+    "name": "Zero Buggers",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 41,
+    "name": "Predator",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 42,
+    "name": "Code Patrol Delta",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 43,
+    "name": "Dragons",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 44,
+    "name": "hack_reva",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 45,
+    "name": "coding ninja",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 46,
+    "name": "Binary",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 47,
+    "name": "ARXP",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 48,
+    "name": "Hack Elite",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 49,
+    "name": "Pradarshane",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 50,
+    "name": "Syntax Sanitizers",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 51,
+    "name": "HackStreetBoys",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 52,
+    "name": "Flash",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 53,
+    "name": "PNS",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 54,
+    "name": "hiriyu",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 55,
+    "name": "Tech_bluff",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 56,
+    "name": "Encephlon",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 57,
+    "name": "Green.io",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 58,
+    "name": "Aurora",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 59,
+    "name": "#include",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 60,
+    "name": "COFFEE AND CODE",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 61,
+    "name": "BEINNOVATIVE",
+    "questions": 1,
+    "points": 10
+  },
+  {
+    "id": 62,
+    "name": "Segmentation Fault",
+    "questions": 1,
+    "points": 10
+  }
+ ]
 
 export default LeaderBoardData;
